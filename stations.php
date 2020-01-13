a47ae0d85f9d315bb3e694434e3315521392d89e
Platinum377 <platinum.mc377@gmail.com>
2020-01-13 16:59:02 +0100

    Ajout fichier PHP et Modif Fichier CSS

---
+++ stations.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Site Agence</title>
+  <link href="style.css" rel="stylesheet">
+  
+</head>
+<body>
+   
+    <?php
+    /**********************************************
+    Nom du script : stations.php
+    Description   : Script se connectant au serveur de base de données                 et à la BD Agence_voyage" pour intéroger la table 
+                    "station" et afficher les résultats dans un tableau HTML
+    Auteur        : M. LETAIEF
+    Version       : 1.0
+    Date          : 13/01/2020
+    *********************************************/
+    ?>
+    
+    <h1> Les Stations </h1>
+    
+</body>
+</html>
